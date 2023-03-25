ce3a48afdb46d137ab74b77283883b9eac0d2e0f
Alan Ricardo <alan.ricardo.silva1@gmail.com>
2023-03-24 21:43:14 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "allocated_storage" {
+  type = number
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "engine" {
+  type = string
+}
+
+variable "engine_version" {
+  type = number
+}
+
+variable "instance_class" {
+  type = string
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type = number
+}
+
+variable "parameter_group_name" {
+  type = string
+}
+
+variable "skip_final_snapshot" {
+  type = bool
+}
